f66eda9fef3c8a346efcadc297402c19cb70fa44
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-24 16:25:42 -0300

    tests creation of an object of class Carro

---
+++ programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/CarroTest.java
@@ -0,0 +1,13 @@
+package dev.alejandrocosta;
+
+public class CarroTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Criando um objeto da classe Carro...");
+        var umCarro = new Carro("branco", "Logan", 45.5);
+        System.out.println("Ok");
+
+	}
+
+}
