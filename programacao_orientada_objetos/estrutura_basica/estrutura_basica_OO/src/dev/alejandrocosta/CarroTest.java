29f12041c5716ab32d289fd9e2f8f9b84601dde8
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-24 18:59:37 -0300

    tests constructor without the specification of capacidadeDoTanque

--- programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/CarroTest.java
+++ programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/CarroTest.java
@@ -7,6 +7,10 @@ public class CarroTest {
 		System.out.println("Criando um objeto da classe Carro...");
         var umCarro = new Carro("branco", "Logan", 45.5);
         System.out.println("Ok");
+        
+        System.out.println("Criando Carro sem especificar capacidade do tanque...");
+        var outroCarro = new Carro("branco", "Logan");
+        System.out.println("Ok");
 
 	}
 
