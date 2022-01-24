2410603bb00a3ff32efbf6b34af0be8aa72579fe
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-24 19:02:21 -0300

    definition of constructor without the specification of capacidadeDoTanque

--- programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/Carro.java
+++ programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/Carro.java
@@ -3,16 +3,25 @@ package dev.alejandrocosta;
 /**
  * Classe de exemplo para aulas sobre estrutura básica da orientação à objetos
 */
-public class Carro {
+public class Carro 
+{
 	
 	private String cor;
 	private String modelo;
 	private double capacidadeDoTanque;
 
-	Carro(String cor, String modelo, double capacidadeDoTanque){
+	Carro(String cor, String modelo, double capacidadeDoTanque)
+	{
 		this.cor = cor;
 		this.modelo = modelo;
 		this.capacidadeDoTanque = capacidadeDoTanque;
+	}
+	
+	Carro(String cor, String modelo)
+	{
+	    this.cor = cor;
+	    this.modelo = modelo;
+	    this.capacidadeDoTanque = 40;
 	}
 	
 }
