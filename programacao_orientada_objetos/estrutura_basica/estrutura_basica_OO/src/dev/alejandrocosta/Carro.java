47373809a135213a62a24f9f0bc69dcff4180ca6
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-24 16:03:43 -0300

    definition of variables cor, modelo, and capacidadeDoTanque as answer to exercise 2

--- programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/Carro.java
+++ programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/Carro.java
@@ -4,5 +4,9 @@ package dev.alejandrocosta;
  * Classe de exemplo para aulas sobre estrutura básica da orientação à objetos
 */
 public class Carro {
+	
+	private String cor;
+	private String modelo;
+	private double capacidadeDoTanque;
 
 }
