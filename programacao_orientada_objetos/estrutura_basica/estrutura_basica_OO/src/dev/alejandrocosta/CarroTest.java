416460e7f4711711a5a70c9ca014b79712b3b631
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-26 08:35:00 -0300

    tests method setCor()

--- programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/CarroTest.java
+++ programacao_orientada_objetos/estrutura_basica/estrutura_basica_OO/src/dev/alejandrocosta/CarroTest.java
@@ -16,13 +16,17 @@ public class CarroTest {
         System.out.println("Valor do tanque cheio: " + umCarro.calcularValorDoTanqueCheio(6.99));
         System.out.println("Ok");
         
-        System.out.println("Alterando o modelo do carro...");
+        System.out.println("Alterando o atributo modelo do carro...");
         umCarro.setModelo("Fluence");
         System.out.println("Modelo alterado com sucesso");
         
         System.out.println("Apresentando valores dos atributos...");
         System.out.println("Apresentação do modelo: " + umCarro.getModelo());
         System.out.println("Ok");
+        
+        System.out.println("Alterando o atributo cor do carro...");
+        umCarro.setCor("red");
+        System.out.println("Cor alterada com sucesso");
 	}
 
 }
