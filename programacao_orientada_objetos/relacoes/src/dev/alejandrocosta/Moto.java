19ebf9cb5a22f0089210f92ac323f0cfbd616fe6
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-27 13:21:55 -0300

    class Moto extends Veiculo. Implementation of the constructor

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/Moto.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/Moto.java
@@ -1,5 +1,18 @@
 package dev.alejandrocosta;
 
-public class Moto {
+public class Moto extends Veiculo{
 
+	private String marca;
+	private String modelo;
+	private int cilindradas;
+	private int capacidadeDoTanque;
+	
+	Moto(String proprietario, String placa, String marca, String modelo, int cilindradas, int capacidadeDoTanque)
+	{
+		super(proprietario, placa);
+		this.marca = marca;
+		this.modelo = modelo;
+		this.cilindradas = cilindradas;
+		this.capacidadeDoTanque = capacidadeDoTanque;
+	}
 }
