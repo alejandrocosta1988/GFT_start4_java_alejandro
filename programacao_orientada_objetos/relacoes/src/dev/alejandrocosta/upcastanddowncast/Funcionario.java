28c75930f991625961ef5a026ccf9eb5d89ae25e
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-27 15:30:07 -0300

    definition of superclass Funcionario

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/upcastanddowncast/Funcionario.java
@@ -0,0 +1,16 @@
+package dev.alejandrocosta.upcastanddowncast;
+
+public class Funcionario
+{
+    private String nome;
+    
+    Funcionario(String nome)
+    {
+    	this.nome = nome;
+    }
+    
+    public getNome()
+    {
+    	return this.nome;
+    }
+}
