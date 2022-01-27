6d571eeeaabf873c82de6316309d4a4e9621da3b
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-27 13:24:28 -0300

    tests constructor of class Moto

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/MotoTest.java
@@ -0,0 +1,13 @@
+package dev.alejandrocosta;
+
+public class MotoTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Criando um objeto da classe Moto...");
+        var umaMoto = new Moto("Alejandro Costa", "NH467J89", "Honda", "Shadow", 750, 18);
+        System.out.println("Ok\n");
+
+	}
+
+}
