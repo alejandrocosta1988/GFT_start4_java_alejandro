471fb8438811d3ea3d2a170554a51d1fbcddea6e
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-27 15:33:57 -0300

    creates an instance of Funcionario and exhibits the name

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/upcastanddowncast/RodarAplicacao.java
@@ -0,0 +1,13 @@
+package dev.alejandrocosta.upcastanddowncast;
+
+public class RodarAplicacao {
+
+	public static void main(String[] args)
+	{
+		
+        Funcionario umFuncionario = new Funcionario("Alejandro Costa");
+        System.out.println(umFuncionario.getNome());
+
+	}
+
+}
