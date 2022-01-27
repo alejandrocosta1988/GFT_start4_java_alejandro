4a51da7e082a0bedacef3c899eb5b1d7335ccf90
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-27 12:57:03 -0300

    start project to demonstrate relationships between classes in OO using Java

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/Veiculo.java
@@ -0,0 +1,13 @@
+package dev.alejandrocosta;
+
+public class Veiculo {
+	
+    private String proprietario;
+    private String placa;
+    
+    Veiculo(String proprietario, String placa)
+    {
+    	this.proprietario = proprietario;
+    	this.placa = placa;
+    }
+}
