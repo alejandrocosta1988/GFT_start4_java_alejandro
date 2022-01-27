6783f203e4fe15cea56ed52be336bd6d7a99bd77
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-27 13:05:30 -0300

    tests method getProprietario()

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/VeiculoTest.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/VeiculoTest.java
@@ -8,6 +8,10 @@ public class VeiculoTest {
         System.out.println("Criando um objeto da classe Veiculo...");
         Veiculo umVeiculo = new Veiculo("Alejandro Costa", "NH467J89");
         System.out.println("Ok\n");
+        
+        System.out.println("Exibindo nome de proprietário de um veículo...");
+        System.out.println("Nome do proprietário: " + umVeiculo.getProprietario());
+        System.out.println("Ok\n");
 
 	}
 
