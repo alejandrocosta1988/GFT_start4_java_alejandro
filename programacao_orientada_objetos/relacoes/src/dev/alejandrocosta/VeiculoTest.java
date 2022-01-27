d91d1864ae24f134a870f7f3daa11edf13208c99
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-27 13:03:01 -0300

    tests constructor of Veiculo

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/VeiculoTest.java
@@ -0,0 +1,14 @@
+package dev.alejandrocosta;
+import Veiculo;
+
+public class VeiculoTest {
+
+	public static void main(String[] args) {
+		
+        System.out.println("Criando um objeto da classe Veiculo...");
+        Veiculo umVeiculo = new Veiculo("Alejandro Costa", "NH467J89");
+        System.out.println("Ok\n");
+
+	}
+
+}
