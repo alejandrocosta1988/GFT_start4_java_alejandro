8bb3ecd6140e0a69bb7395f2c70ce4f7b35b1656
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-28 07:25:39 -0300

    tests constructor

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/TestEndereco.java
@@ -0,0 +1,12 @@
+package dev.alejandrocosta.association;
+
+public class TestEndereco {
+
+	public static void main(String[] args) {
+		
+        Endereco umEndereco = new Endereco("Rua Quintino Bocaiuva", "Xique-Xique", "Bahia");
+        System.out.println("Ok");
+
+	}
+
+}
