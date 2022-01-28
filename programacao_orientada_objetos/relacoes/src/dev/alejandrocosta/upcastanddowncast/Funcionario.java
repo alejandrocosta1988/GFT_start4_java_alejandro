3655eab33091a003913fea819698c52cce5527f7
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-28 06:42:19 -0300

    defines attribute salario and method getSalario()

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/upcastanddowncast/Funcionario.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/upcastanddowncast/Funcionario.java
@@ -3,14 +3,21 @@ package dev.alejandrocosta.upcastanddowncast;
 public class Funcionario
 {
     private String nome;
+    private double salario;
     
-    Funcionario(String nome)
+    Funcionario(String nome, double salario)
     {
     	this.nome = nome;
+    	this.salario = salario;
     }
     
     public String getNome()
     {
     	return this.nome;
+    }
+    
+    public double getSalario()
+    {
+    	return this.salario;
     }
 }
