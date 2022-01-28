e2f1a8b5321a9a67cd65ebc986db2a425f037f6d
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-28 08:30:15 -0300

    implements class Pessoa

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/Pessoa.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/Pessoa.java
@@ -2,4 +2,13 @@ package dev.alejandrocosta.association;
 
 public class Pessoa {
 
+	private String nome;
+	private Endereco endereco;
+	
+	Pessoa(String nome, Endereco endereco)
+	{
+		this.nome = nome;
+		this.endereco = endereco;
+	}
+	
 }
