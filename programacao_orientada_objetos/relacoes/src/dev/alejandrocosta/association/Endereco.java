d80cc52d37c64fd505f7a548a75b1615bcc82ad9
Alejandro Costa <alejandroesfc@gmail.com>
2022-01-28 07:25:12 -0300

    implements class Endereco

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/Endereco.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/Endereco.java
@@ -1,5 +1,15 @@
 package dev.alejandrocosta.association;
 
 public class Endereco {
-
+	
+    private String rua;
+    private String cidade;
+    private String estado;
+    
+    Endereco(String rua, String cidade, String estado)
+    {
+    	this.rua = rua;
+    	this.cidade = cidade;
+    	this.estado = estado;
+    }
 }
