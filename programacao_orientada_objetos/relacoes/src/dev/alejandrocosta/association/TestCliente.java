52767fede34436d3c4859e41866a64605536630d
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-04 08:02:30 -0300

    tests constructor of class Cliente

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/TestCliente.java
@@ -0,0 +1,14 @@
+package dev.alejandrocosta.association;
+
+public class TestCliente {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Criando um cliente...");
+		Cliente clienteUm = new Cliente("Alejandro Costa", "Villa Caputti", "Xique-Xique", "Bahia");
+		System.out.println("Ok");
+		System.out.println(clienteUm);
+
+	}
+
+}
