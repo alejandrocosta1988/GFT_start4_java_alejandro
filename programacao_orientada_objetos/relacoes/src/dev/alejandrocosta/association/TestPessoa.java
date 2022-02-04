c784c4d525ff269e38b5d951a8e3006f1f6e3704
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-04 07:49:59 -0300

    tests constructor that accepts nome, rua, cidade, estado

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/TestPessoa.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/TestPessoa.java
@@ -9,6 +9,11 @@ public class TestPessoa {
         Pessoa umaPessoa = new Pessoa("Alejandro Costa", umEndereco);
         System.out.println("Ok");
         System.out.println(umaPessoa);
+        
+        System.out.println("Criando uma pessoa com construtor que aceita nome, rua, cidade e estado...");
+        Pessoa pessoaDois = new Pessoa("Jana R Santana", "Villa Caputti", "Xique-Xique", "Bahia");
+        System.out.println("Ok");
+        System.out.println(pessoaDois);
 
 	}
 
