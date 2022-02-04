a45d64ddbd0f55a5abed1e21edcb5f8a3b40dc28
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-04 08:02:00 -0300

    implements subclass Cliente. Superclass is Pessoa

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/Cliente.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/Cliente.java
@@ -1,5 +1,14 @@
 package dev.alejandrocosta.association;
 
-public class Cliente {
-
+public class Cliente extends Pessoa
+{
+    private int numeroDeCompras;
+    
+    Cliente(String nome, String rua, String cidade, String estado)
+    {
+        super(nome, rua, cidade, estado);
+        this.numeroDeCompras = 0;
+    }
+    
+    
 }
