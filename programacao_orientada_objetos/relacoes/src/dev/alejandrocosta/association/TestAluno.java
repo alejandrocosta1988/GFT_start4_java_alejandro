336208138d1aeb19a64fb74990373efa4c084e51
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-05 20:18:53 -0300

    tests constructor of Aluno

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/TestAluno.java
@@ -0,0 +1,13 @@
+package dev.alejandrocosta.association;
+
+public class TestAluno {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Criando um aluno...");
+		Aluno alunoUm = new Aluno("Alejandro Costa", "Villa Caputi", "Xique-Xique", "Bahia", "Engenharia de Software");
+		System.out.println("Ok");
+
+	}
+
+}
