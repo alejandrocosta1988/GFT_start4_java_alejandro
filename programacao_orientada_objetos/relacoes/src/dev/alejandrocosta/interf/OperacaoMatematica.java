cbd21b463fbe0357ab0fb914ec7bb49cd9b15f09
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-06 16:24:54 -0300

    defines four basic operations in the interface

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/interf/OperacaoMatematica.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/interf/OperacaoMatematica.java
@@ -1,5 +1,13 @@
 package dev.alejandrocosta.interf;
 
 public interface OperacaoMatematica {
+	
+	public void soma(double primeiroNumero, double segundoNumero);
+	
+	public void subtracao(double primeiroNumero, double segundoNumero);
+	
+	public void multiplicacao(double primeiroNumero, double segundoNumero);
+	
+	public void divisao(double primeiroNumero, double segundoNumero);
 
 }
