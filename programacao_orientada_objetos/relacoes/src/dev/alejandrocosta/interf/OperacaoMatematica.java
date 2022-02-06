81533f3e6f63fb07343995b5cf2e93516666af81
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-06 16:28:18 -0300

    comments about the definition of methods in interface

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/interf/OperacaoMatematica.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/interf/OperacaoMatematica.java
@@ -2,6 +2,7 @@ package dev.alejandrocosta.interf;
 
 public interface OperacaoMatematica {
 	
+	// Nas interfaces podemos trabalhar com métodos sem as chaves, utilizando apenas a assinatura. A partir do Java 8, podemos incluir métodos default.
 	public void soma(double primeiroNumero, double segundoNumero);
 	
 	public void subtracao(double primeiroNumero, double segundoNumero);
