bcb2bdb0543462101f7eafa382f5c284aae8eb85
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-06 16:38:05 -0300

    comments about interfaces

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/interf/OperacaoMatematica.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/interf/OperacaoMatematica.java
@@ -3,6 +3,7 @@ package dev.alejandrocosta.interf;
 public interface OperacaoMatematica {
 	
 	// Nas interfaces podemos trabalhar com métodos sem as chaves, utilizando apenas a assinatura. A partir do Java 8, podemos incluir métodos default.
+	// Uma interface obriga um terceiro a implementar todos os métodos contidos nela.
 	public void soma(double primeiroNumero, double segundoNumero);
 	
 	public void subtracao(double primeiroNumero, double segundoNumero);
