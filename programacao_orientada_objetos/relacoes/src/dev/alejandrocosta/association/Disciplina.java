b1ade36a1870df90ecc1d35e441bfa1762362d7c
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-06 16:18:34 -0300

    implements class Disciplina

--- programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/Disciplina.java
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/association/Disciplina.java
@@ -1,5 +1,13 @@
 package dev.alejandrocosta.association;
 
-public class Disciplina {
-
+public class Disciplina 
+{
+	private String nomeDoComponenteCurricular;
+	//Associação do tipo: agregação. Aluno não depende de Disciplina para existir.
+    private Aluno[] alunosMatriculados;
+    
+    public Disciplina(String nomeDoComponenteCurricular)
+    {
+    	this.nomeDoComponenteCurricular = nomeDoComponteCurricular;
+    }
 }
