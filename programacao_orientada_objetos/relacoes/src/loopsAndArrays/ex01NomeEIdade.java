f17c247f882a98af99b789d0d995e393527eb387
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-09 18:15:40 -0300

    exercise 01

---
+++ programacao_orientada_objetos/relacoes/src/loopsAndArrays/ex01NomeEIdade.java
@@ -0,0 +1,24 @@
+package loopsAndArrays;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+public class ex01NomeEIdade {
+	
+	public static void main(String[] args)
+	{
+		
+		public Scanner in = new Scanner(System.in);
+		
+		public String nome;
+		public int idade;
+		
+		
+		
+	}
+	
+}
