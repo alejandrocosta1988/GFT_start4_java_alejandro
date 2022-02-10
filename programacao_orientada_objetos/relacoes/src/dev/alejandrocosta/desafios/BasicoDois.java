0826421fdddff4827e6b8f431686ed8a09f6004f
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-10 06:15:15 -0300

    creates package for the GFT bootcamp challenges, and creates file for challenge 2 of basic level

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/desafios/BasicoDois.java
@@ -0,0 +1,20 @@
+package dev.alejandrocosta.desafios;
+
+import java.util.Scanner;
+
+/**
+ * Desafios
+ * @author alejandro_costa
+ * Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+ * Entrada 
+ * Você receberá 1 valor inteiro N, onde N > 0.
+ * Saída 
+ * Exiba todos os números pares até o valor de entrada, sendo um em cada linha.
+ */ 
+public class BasicoDois {
+    
+	public static void main(String[] args)
+	{
+		
+	}
+}
