010940466361d378901e87873c15d616e82db0dc
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-10 17:11:56 -0300

    Intermediate challenge 01

---
+++ programacao_orientada_objetos/relacoes/src/dev/alejandrocosta/desafios/IntermediarioUm.java
@@ -0,0 +1,16 @@
+package dev.alejandrocosta.desafios;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class IntermediarioUm {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+    	double A = leitor.nextDouble();
+    	double B = leitor.nextDouble();
+    	
+	}
+
+}
