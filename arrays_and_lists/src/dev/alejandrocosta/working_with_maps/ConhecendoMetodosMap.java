71bc0756a5d41bff24f8490227159f24e4934e7b
Alejandro Costa <alejandroesfc@gmail.com>
2022-02-22 05:57:03 -0300

    demonstration of several methods that may be used in Map

--- arrays_and_lists/src/dev/alejandrocosta/working_with_maps/ConhecendoMetodosMap.java
+++ arrays_and_lists/src/dev/alejandrocosta/working_with_maps/ConhecendoMetodosMap.java
@@ -3,6 +3,7 @@ package dev.alejandrocosta.working_with_maps;
 import java.util.Map;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Set;
 
 public class ConhecendoMetodosMap {
 
@@ -44,7 +45,25 @@ public class ConhecendoMetodosMap {
 		System.out.println();
 		
 		System.out.println("Carro mais econômico...");
+		double consumoMaisEficiente = Collections.max(carrosPopulares.values());
+		Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet(); // O método .entrySet() retorna um Set com a implementação da interface Entry
+		String modeloMaisEficiente = "";
+		for (Map.Entry<String, Double> entry : entries)
+		{
+			if (entry.getValue().equals(consumoMaisEficiente)) modeloMaisEficiente = entry.getKey();
+		}
+		System.out.println("Modelo mais eficiente: " + modeloMaisEficiente + " - " + consumoMaisEficiente + " km/L");
+		System.out.println();
 		
+		System.out.println("Modelo menos econômico...");
+		double consumoMenosEficiente = Collections.min(carrosPopulares.values());
+		Set<Map.Entry<String, Double>> newEntry = carrosPopulares.entrySet();
+		String modeloMenosEficiente = "";
+		for (Map.Entry<String, Double> modelo : newEntry)
+		{
+			if (modelo.getValue().equals(consumoMenosEficiente)) modeloMenosEficiente = modelo.getKey();
+			System.out.println("Modelo menos eficiente: " + modeloMenosEficiente + " - " + consumoMenosEficiente + " km/L");
+		} // inserindo a impressão dentro do if, se houver mais de um modelo com o mesmo consumo, ambos serão impressos. 
 		System.out.println();
 		
 		
