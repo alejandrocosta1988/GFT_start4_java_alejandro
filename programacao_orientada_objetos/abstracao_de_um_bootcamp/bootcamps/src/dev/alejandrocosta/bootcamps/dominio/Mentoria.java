d5ab4a46d4d8cc3d6379474ceb81e2f8bb15dd81
Alejandro Costa <alejandroesfc@gmail.com>
2022-03-02 10:39:34 -0300

    creates superclass Conteudo. Refactoring of child class Mentoria to extend Conteudo

--- programacao_orientada_objetos/abstracao_de_um_bootcamp/bootcamps/src/dev/alejandrocosta/bootcamps/dominio/Mentoria.java
+++ programacao_orientada_objetos/abstracao_de_um_bootcamp/bootcamps/src/dev/alejandrocosta/bootcamps/dominio/Mentoria.java
@@ -2,31 +2,11 @@ package dev.alejandrocosta.bootcamps.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo
+{
 	
-	private String titulo;
-	private String descricao;
 	private LocalDate data;
 	
-	public String getTitulo() 
-	{
-		return titulo;
-	}
-	
-	public void setTitulo(String titulo) 
-	{
-		this.titulo = titulo;
-	}
-	
-	public String getDescricao() 
-	{
-		return descricao;
-	}
-	
-	public void setDescricao(String descricao) 
-	{
-		this.descricao = descricao;
-	}
 	
 	public LocalDate getData() 
 	{
@@ -41,7 +21,13 @@ public class Mentoria {
 	@Override
 	public String toString() 
 	{
-		return "Mentoria [titulo = " + titulo + ", descricao = " + descricao + ", data = " + data + "]";
+		return "Mentoria [titulo = " + getTitulo() + ", descricao = " + getDescricao() + ", data = " + data + "]";
+	}
+	
+	@Override
+	public double calcularXP()
+	{
+		return XP_PADRAO + 20;
 	}
 	
 	
