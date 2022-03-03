b86c50fbf55cee4a017014bda06db03b9dc15b35
Alejandro Costa <alejandroesfc@gmail.com>
2022-03-03 20:55:10 -0300

    implements methods in classes Bootcamp and Dev

--- programacao_orientada_objetos/abstracao_de_um_bootcamp/bootcamps/src/dev/alejandrocosta/bootcamps/dominio/Dev.java
+++ programacao_orientada_objetos/abstracao_de_um_bootcamp/bootcamps/src/dev/alejandrocosta/bootcamps/dominio/Dev.java
@@ -2,6 +2,7 @@ package dev.alejandrocosta.bootcamps.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev
@@ -12,17 +13,29 @@ public class Dev
 	
 	public void inscreverBootcamp(Bootcamp bootcamp)
 	{
-		
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
 	}
 	
 	public void progredir() 
 	{
-		
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+		if ( conteudo.isPresent() )
+		{
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get());
+		} else
+		{
+			System.err.println("Você não está inscrito em nenhum conteúdo.");
+		}
 	}
 	
-	public void calcularTotalXP() 
+	public double calcularTotalXP() 
 	{
-		
+		return this.conteudosInscritos
+				.stream()
+				.mapToDouble(Conteudo::calcularXP)
+				.sum();
 	}
 
 	public String getNome() 
